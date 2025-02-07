c52b69df50fe8a74542e9a82102f4f7398ffb9ec
andres-dalisay <21106151+andres-dalisay@users.noreply.github.com>
2025-02-07 16:22:04 +0800

    solved 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const array = string.split('');
+    let reversed = "";
 
+    while (array.length > 0) {
+        reversed += array.pop();
+    }
+    
+    return reversed;
 };
 
+reverseString('hello');
 // Do not edit below this line
 module.exports = reverseString;
