e37318551fdbefb4d911c144b49509dcc1b6e280
andres-dalisay <21106151+andres-dalisay@users.noreply.github.com>
2025-02-07 16:49:08 +0800

    solved 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value, ...values) {
+    const valueArr = [value].concat(values);
+
+    for (let i = 0; i < valueArr.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === valueArr[i]) {
+                array.splice(j, 1);
+                i--;
+                console.log(array);
+            }
+        }        
+    }
+
+    return array;
+
+    // console.log(array);
 };
 
 // Do not edit below this line
